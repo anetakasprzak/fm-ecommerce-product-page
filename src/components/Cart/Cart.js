cd96c89561a3ad0d9039659a3e96353da8d3f79c
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-05-02 17:06:47 +0100

    cart jsx

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -16,11 +16,65 @@ export const Icon = styled.button`
   }
 `;
 
-export const Box = styled.div`
-  height: 25.6rem;
+export const CartBox = styled.div`
+  min-height: 25.6rem;
   width: 36rem;
+  padding: 2.4rem;
   position: absolute;
   top: 5.5rem;
   right: -5rem;
-  background-color: red;
+  background-color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0px 20px 50px -20px rgba(29, 32, 38, 0.5);
+`;
+
+export const Heading = styled.h4`
+  color: #1d2026;
+  font-weight: 700;
+  padding-bottom: 2.5rem;
+  border-bottom: 1px solid #e4e9f2;
+`;
+
+export const ItemBox = styled.div`
+  padding-top: 2.4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Image = styled.div`
+  img {
+    height: 5rem;
+    border-radius: 0.4rem;
+  }
+`;
+
+export const TextBox = styled.div``;
+
+export const Description = styled.p``;
+
+export const PriceBox = styled.p`
+  display: flex;
+`;
+
+export const Price = styled.p``;
+
+export const Quantity = styled.p``;
+
+export const Sum = styled.p``;
+
+export const DeleteIcon = styled.div`
+  svg {
+    fill: #c3cad9;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:hover {
+      fill: #ff7e1b;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  border: none;
 `;
